bdfdb7cd80290cdfd102e2e7bb0ebccf48b2e2e9
nishatoma <nishatoma@gmail.com>
2019-02-18 02:27:59 -0800

    Inject service into another service

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,7 @@
+import { LoggingService } from "./logging.service";
+import { Injectable } from "@angular/core";
+
+@Injectable()
 export class AccountsService {
     accounts = [
         {
@@ -14,11 +18,20 @@ export class AccountsService {
         }
       ];
 
+      // We are going to inject LoggingService into this service class
+      // In order to do so, this class needs to have some metadata, so
+      // we attach the @Injectable decorator at the top.
+      constructor(private loggingService: LoggingService) {
+
+      }
+
       addAccount(name: string, status: string) {
         this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status);
       }
 
       updateStatus(id: number, status: string) {
         this.accounts[id].status = status;
+        this.loggingService.logStatusChange(status);
       }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -20,6 +20,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
